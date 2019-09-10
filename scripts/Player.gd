b43f57eb1dad97d18d91df78f4f654993571df43
Robin Andersson <robin182andersson@gmail.com>
2019-09-10 11:34:23 +0200

    Added jumping and gravity

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,23 +1,30 @@
 extends KinematicBody2D
 
+const SPEED = 60
+const GRAVITY = 10
+const JUMP_POWER = -250
+const FLOOR = Vector2(0, -1)
+
 var velocity = Vector2()
+var on_ground = false
 
 func _ready():
 	$AnimatedSprite.play("idle")
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 30
+		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -30
+		velocity.x = -SPEED
 	else:
 		velocity.x = 0
 	
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -30
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 30
-	else:
-		velocity.y = 0
+	if Input.is_action_pressed("ui_up") and on_ground:
+		velocity.y = JUMP_POWER
+		on_ground = false
 	
-	move_and_slide(velocity)
+	velocity.y += GRAVITY
+	
+	on_ground = is_on_floor()
+	
+	velocity = move_and_slide(velocity, FLOOR)
